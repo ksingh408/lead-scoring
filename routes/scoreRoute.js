9832579246c504b4182e214aef9d4cc266a51709
ksingh408 <kunalsinghxy@gmail.com>
2025-09-20 02:22:11 +0530

    feat: add score routes exportCSV

--- routes/scoreRoute.js
+++ routes/scoreRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { runScoring, getResults } = require('../controllers/scoreController');
+const { runScoring, getResults,exportResultsCSV } = require('../controllers/scoreController');
 
 router.post('/:uploadId', runScoring);
 router.get('/results', getResults);
+router.get('/exportCSV',exportResultsCSV)
 
 module.exports = router;
